4062c30204a8a6f3ff54770f225e987c43160ead
Paul Josué Michel <d47303647@instituto.itr.edu.ar>
2023-08-03 16:54:19 -0300

    desafio 1 terminado :)

--- desafio0.js
+++ desafio0.js
@@ -1,3 +1,5 @@
+// Codificador Cesar
+
 let alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 let message = "YO HE LOGRADO ENCENDER UNA CERILLA";
 let rotator1 = 2;
@@ -8,9 +10,9 @@ message = message.split(/\s+/).join('');
 
 
 for(let i=0; i<message.length; i+=2){
-    response += alphabet[(alphabet.indexOf([message[i]])+rotator1)%27];
+    response += alphabet[(alphabet.indexOf([message[i]])+rotator1)%alphabet.length];
     if(message.length-1 >= i+1){
-      response += alphabet[(alphabet.indexOf([message[i+1]])+rotator2)%27];
+      response += alphabet[(alphabet.indexOf([message[i+1]])+rotator2)%alphabet.length];
     }
   }
     
---
+++ desafio1.js
@@ -0,0 +1,14 @@
+let originalNum = 60;
+let num = 60;
+let primos = [2,3,5,7,11,13];
+let sum = 0;
+
+for(let i=0; i<primos.length; i++){
+    if(num%primos[i] == 0){
+        sum += num/primos[i];
+        num = num/primos[i];
+        i = -1;
+    }
+}
+
+console.log(sum+originalNum);
