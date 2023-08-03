d39badfdcd7276b656e8a8ca08993e66673286dd
Paul Josué Michel <d47303647@instituto.itr.edu.ar>
2023-08-03 11:29:06 -0300

    terminado desafio 0

---
+++ desafio_0/codificador.js
@@ -0,0 +1,18 @@
+let alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+let message = "YO HE LOGRADO ENCENDER UNA CERILLA";
+let rotator1 = 2;
+let rotator2 = 3;
+let response = "";
+
+message = message.split(/\s+/).join('');
+
+
+for(let i=0; i<message.length; i+=2){
+    response += alphabet[(alphabet.indexOf([message[i]])+rotator1)%27];
+    if(message.length-1 >= i+1){
+      response += alphabet[(alphabet.indexOf([message[i+1]])+rotator2)%27];
+    }
+  }
+    
+  
+  console.log(response);
