88575d54f747bbdfedd3d13b64aba232c85ee27b
Paul Josué Michel <d47303647@instituto.itr.edu.ar>
2023-09-01 12:16:56 -0300

    comenzando ejercicio <gravitas>

--- desafio0.cpp
+++ desafio0.cpp
@@ -12,17 +12,19 @@ int key2 = 3;
 string response = "";
 
 int main() {
-    message.erase(remove_if(message.begin(), message.end(), isspace), message.end());
+    // message.erase(remove_if(message.begin(), message.end(), ::isspace), message.end());
     
-    for(int i=0; i<message.size(); i+=N){
-        int index = alphabet.find(i);
-        response += alphabet[(index + key1) % alphabet.size()];
+    for(unsigned int i=0; i<message.size(); i+=N){
+        if (i != ' ') {
+            int index = alphabet.find(message[i]);
+            response += alphabet[(index + key1) % alphabet.size()];
 
-        if(message.size()-1 >= i+1){
-            response += alphabet[(index + key2) % alphabet.size()];
+            if(message.size()-1 >= i+1){
+                response += alphabet[(index + key2) % alphabet.size()];
+            }
         }
     }
 
     cout << response << '\n';
     return 0;
-}
+} 
---
+++ gravitas.js
@@ -0,0 +1,9 @@
+let g = [[".",".","<",">",".","A"],
+        [".",".","A","A","A","V"],
+        [".",".","V","V","V","."]];
+
+function gravitas(g){
+
+}
+
+console.log(gravitas(g));
