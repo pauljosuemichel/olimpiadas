8cce8bda8fe375e0f835d017dc9156f14f830067
Marcelo y Verónica Michel <d47303647@alumnos.itr.edu.ar>
2023-09-04 21:19:47 -0300

    ya tengo una base para el gravitas

--- gravitas.js
+++ gravitas.js
@@ -1,9 +1,16 @@
 let g = [[".",".","<",">",".","A"],
-        [".",".","A","A","A","V"],
+        ["<",">","A","A","A","V"],
         [".",".","V","V","V","."]];
 
-function gravitas(g){
-
+function gravitas(){
+    for(let x=0; x<g.length; x++){ // filas
+        for(let y=0; y<g.length; y++){ // columnas
+            if(g[x][y] == "V" && g[x][y+1] == "."){
+                g[x][y] = ".";
+                g[x][y+1] = "V";
+            }
+        }
+    }
 }
 
-console.log(gravitas(g));
+console.table(g);
