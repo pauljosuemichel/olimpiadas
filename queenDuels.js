2bfa8191a382c2e1e7d0c8026e0dc99a71ca729b
Marcelo y Verónica Michel <d47303647@alumnos.itr.edu.ar>
2023-09-13 21:38:16 -0300

    empezando duelo de reinas

--- queenDuels.js
+++ queenDuels.js
@@ -1,4 +1,34 @@
 let tablero = [[".",".","♛",".","♛"],
                ["♛",".",".",".","♛"],
                [".",".","♛","♛","."],
-               [".","♛",".",".","♛"]];
+               [".","♛",".","♛","."]
+               [".","♛",".","♛","."]];
+let board = 5;
+let queens = 8;
+let q1 = [1,3];
+let q2 = [1,5];
+let q3 = [2,1];
+let q4 = [2,5];
+let q5 = [3,3];
+let q6 = [3,4];
+let q7 = [4,2];
+let q8 = [4,4];
+let q9 = [5,2];
+let q10 = [5,4];
+
+let c1 = 0;
+let c2 = 0;
+let c3 = 0;
+let c4 = 0;
+let c5 = 0;
+let c6 = 0;
+let c7 = 0;
+let c8 = 0;
+let c9 = 0;
+let c10 = 0;
+
+function conflictos(){
+    for(let i; i<queens; i++){
+        
+    }
+}
